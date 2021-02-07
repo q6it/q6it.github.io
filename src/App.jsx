fe60f42e01c60c4de6e78412799d64a2e59e945a
Andrei Sittšenko <andrei.sittsenko@gmail.com>
2021-02-07 20:43:35 +0200

    sidebar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
-import Table from "./components/Table";
+// import logo from './logo.svg';
+// import Table from './components/Table';
+import { Grid } from 'semantic-ui-react';
+
+import Ticker from './components/Ticker';
+import Sidebar from './components/Sidebar/Sidebar';
+
 import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    //     <p>
-    //       {/* Edit <code>src/App.jsx</code> and save to reload. */}
-    //     </p>
-    //   </header>
-    // </div>
-    <div>
-      <Table />
+    <div className="App">
+      <Grid columns={3}>
+        <Grid.Column>
+          <Sidebar />
+        </Grid.Column>
+        <Grid.Row>
+          <Grid.Column></Grid.Column>
+          <Grid.Column>
+            <Ticker />
+          </Grid.Column>
+          <Grid.Column></Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
