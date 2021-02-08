6c9c6d6dbda4454e173c29623bbe67dd88b55199
Andrei Sittšenko <andrei.sittsenko@gmail.com>
2021-02-08 13:26:42 +0200

    gird layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 // import Table from './components/Table';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Image } from 'semantic-ui-react';
 
 import Ticker from './components/Ticker';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -11,18 +11,28 @@ import 'semantic-ui-css/semantic.min.css';
 function App() {
   return (
     <div className="App">
-      <Grid columns={3}>
-        <Grid.Column>
-          <Sidebar />
+      <Grid>
+        <Grid.Column mobile={16} tablet={8} computer={4}>
+          <Sidebar width={200} height="100vh">
+            <h1>item 1</h1>
+            <h1>item 2</h1>
+            <h1>item 3</h1>
+          </Sidebar>
         </Grid.Column>
-        <Grid.Row>
-          <Grid.Column></Grid.Column>
-          <Grid.Column>
+        <Grid.Column mobile={16} tablet={8} computer={5}>
+          <Grid.Row>
+            <h1>Create order</h1>
+          </Grid.Row>
+          <Grid.Row>
             <Ticker />
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-      </Grid>
+          </Grid.Row>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={8} computer={5}>
+          <Grid.Row>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Row>
+        </Grid.Column>
+      </Grid> 
     </div>
   );
 }
